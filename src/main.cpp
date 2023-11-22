ac9b0156607be428a637b12e4e1c84606e9298be
zhibinhuosheng <1784297584@qq.com>
2023-11-22 18:43:50 +0800

    add file

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "..\include\main.h"
+
+int main()
+{
+    std::cout << "hello world1" << std::endl;
+    std::cout << "hello world2" << std::endl;
+    std::cout << sum(1, 3) << std::endl;
+    system("pause");
+    return 0;
+}
